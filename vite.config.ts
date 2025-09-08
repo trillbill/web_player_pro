4cdd42dfc684804b54ca488960af27875bfeb6f5
Will Finnegan <wfinnegan13@gmail.com>
2025-09-08 13:56:31 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), crossOriginIsolation()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
